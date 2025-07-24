9b678f6aef5abbeb69addac0d315d3152f119d6c
Ben Purinton <ben.purinton@gmail.com>
2025-07-24 07:53:20 -0700

    Fix database naming

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module PhotogramIndustrialAuthorization
+module PhotogramAjax
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
